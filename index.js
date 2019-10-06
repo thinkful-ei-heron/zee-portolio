ca82fb913047b3046b31e22387c5ef49bb305314
Zee Celest <zeecelest@gmail.com>
2019-10-06 16:20:37 -0400

    added function stub and pseudo code

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+//this function moves user to section of page per link clicked
 const aboutZee = function (obj) {
     console.log(aboutZee);
   $('#js-nav-link').on('click', function (event) {
     event.preventDefault();
     }
 };
+
+//this function will allow user to return to home/top of screen
+const handleReturnHomeButton = function () {
+
+};
+
