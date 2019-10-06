2f666052365c7763f3546c788300bba25906d53e
Zee Celest <zeecelest@gmail.com>
2019-10-06 09:39:28 -0400

    added store.js

---
+++ store.js
@@ -0,0 +1,17 @@
+const zeeProjects = [
+  {
+    thumbnail: 'Quizapp.png',
+    title: 'Outerspace Quiz App',
+    desc: 'An app that tests the users knowledge of space.',
+    githubLink: 'https://github.com/thinkful-ei-heron/zee_shannon_quiz_app',
+    livelink: 'https://github.com/thinkful-ei-heron/zee_shannon_quiz_app/'
+  },
+  {
+    thumbnail: '',
+    title: '',
+    desc: '',
+    githubLink: '',
+    livelink: ''
+  }
+
+];
