a24f22e1395e3b56db8c7ac957aeec0c6cef0366
Zee Celest <zeecelest@gmail.com>
2019-10-06 19:42:13 -0400

    adding css

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //this function moves user to section of page per link clicked
 const aboutZee = function (obj) {
-    console.log(aboutZee);
+  console.log(aboutZee);
   $('#js-nav-link').on('click', function (event) {
     event.preventDefault();
-    }
+  });
+  aboutZee();
 };
 
 //this function will allow user to return to home/top of screen
